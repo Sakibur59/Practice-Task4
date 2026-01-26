1a21caf24e1033295f886a6c2fdf1557146e09ea
Sakibur59 <sakiburrahman5978@gmail.com>
2026-01-26 22:02:16 +0600

    Multipication Table

---
+++ problem5.js
@@ -0,0 +1,12 @@
+// let a=9;
+// let total=0;
+// for(let i =1;i<=10;i++){
+//     total=a*i;
+//     console.log(total);
+// }
+
+let f = 9;
+
+for(let i =1;i<=10;i++){
+    console.log(`${f} * ${i} = ${f*i}`)
+}
