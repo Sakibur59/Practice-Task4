792691fe639b4c058cd209d4b430c7de8f69441c
Sakibur59 <sakiburrahman5978@gmail.com>
2026-01-26 21:44:29 +0600

    sum of even-odd number

---
+++ problem3.js
@@ -0,0 +1,17 @@
+let sum = 0;
+
+for(let i = 91;i<=129;i+=2){
+    console.log(i);
+    sum = sum+i;
+}
+console.log("Sum of all Odd number:",sum);
+
+
+let total= 0;
+
+for(let a = 52;a<85;a+=2){
+    console.log(a);
+    total = total+a;
+}
+
+console.log("Sum of all Even number:",total);
