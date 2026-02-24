d4cd56fc4e5491c995c4851e047f6ad58791c64d
Sakibur59 <sakiburrahman5978@gmail.com>
2026-02-24 22:11:59 +0600

    prb-11

---
+++ problem11.js
@@ -0,0 +1,11 @@
+const number = [12,11,3,4];
+
+const student = {
+    name:'fardin',
+    age:23
+};
+
+
+// console.log(typeof number);
+console.log(Array.isArray(number));
+console.log(typeof student);
